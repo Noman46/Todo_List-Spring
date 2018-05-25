641e153a4e3dd1270fe00ba5d7431e437b4bd44a
Noman46 <nomanibrahim7@gmail.com>
2018-05-25 18:17:29 +0600

    Domain class "user" and database is ready

--- src/main/java/com/noman/todolist/config/SpringRootConfig.java
+++ src/main/java/com/noman/todolist/config/SpringRootConfig.java
@@ -19,7 +19,7 @@ public class SpringRootConfig {
     public BasicDataSource getDataSource(){
         BasicDataSource ds = new BasicDataSource();
         ds.setDriverClassName("org.h2.Driver");
-        ds.setUrl("jdbc:h2:tcp://localhost/~/capp_db");
+        ds.setUrl("jdbc:h2:tcp://localhost/~/todo");
         ds.setUsername("sa");
         ds.setPassword("");
         ds.setMaxTotal(2);
---
+++ src/main/java/com/noman/todolist/domain/User.java
@@ -0,0 +1,69 @@
+package com.noman.todolist.domain;
+
+/**
+ *
+ * @author Noman Ibrahim
+ */
+public class User {
+
+    private Integer userId;
+    private String name;
+    private String phone;
+    private String loginName;
+    private String password;
+    private String loginStatus;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLoginStatus() {
+        return loginStatus;
+    }
+
+    public void setLoginStatus(String loginStatus) {
+        this.loginStatus = loginStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "userId=" + userId + ", name=" + name + ", phone=" + phone + ", loginName=" + loginName + ", password=" + password + ", loginStatus=" + loginStatus + '}';
+    }
+
+}
