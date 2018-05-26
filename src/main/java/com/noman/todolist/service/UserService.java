47edac37b1064912e264dfd2add716c61dcd6068
Noman46 <nomanibrahim7@gmail.com>
2018-05-27 00:35:08 +0600

    Service interface created

---
+++ src/main/java/com/noman/todolist/service/UserService.java
@@ -0,0 +1,13 @@
+
+package com.noman.todolist.service;
+
+import com.noman.todolist.domain.User;
+
+/**
+ *
+ * @author Noman Ibrahim
+ */
+public interface UserService {
+    public void register(User u);
+      
+}
