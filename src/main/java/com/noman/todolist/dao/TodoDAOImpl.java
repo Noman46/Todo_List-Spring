400e2878a91e17eedcf329b48b8f72a2fc47c4e2
Noman46 <nomanibrahim7@gmail.com>
2018-06-11 02:08:21 +0600

    TodoDAO Save method is done

---
+++ src/main/java/com/noman/todolist/dao/TodoDAOImpl.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.noman.todolist.dao;
+
+import com.noman.todolist.domain.Todo;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Noman Ibrahim
+ */
+@Repository
+public class TodoDAOImpl extends BaseDAO implements TodoDAO {
+
+    @Override
+    public void save(Todo todo) {
+        String sql = "INSERT INTO todo (todoId,month,day,year,title,description,priority) VALUES (:todoId, :day, :month, :year, :title, :description, :priority)";
+        Map m = new HashMap();
+        m.put("todoId", todo.getTodoId());
+        m.put("month", todo.getMonth());
+        m.put("day", todo.getDay());
+        m.put("year", todo.getYear());
+        m.put("title", todo.getTitle());
+        m.put("description", todo.getDescription());
+        m.put("priority", todo.getPriority());
+        
+        SqlParameterSource ps = new MapSqlParameterSource(m);
+        KeyHolder kh = new GeneratedKeyHolder();
+        getNamedParameterJdbcTemplate().update(sql, ps, kh);
+        Integer todoId = kh.getKey().intValue();
+        todo.setTodoId(todoId);
+        
+
+    }
+
+    @Override
+    public void update(Todo todo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Todo todo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Integer todoId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Todo findById(Integer todoId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Todo> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Todo> findByProperty(String propName, Object propValue) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
