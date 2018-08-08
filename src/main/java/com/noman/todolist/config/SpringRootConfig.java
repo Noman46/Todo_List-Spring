0c29f5c5a1d1eabd7a4afb14ef988dd42a9e9c71
Noman46 <nomanibrahim7@gmail.com>
2018-08-09 01:40:42 +0600

    Almost done

--- src/main/java/com/noman/todolist/config/SpringRootConfig.java
+++ src/main/java/com/noman/todolist/config/SpringRootConfig.java
@@ -14,7 +14,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = {"com.noman.todolist.dao","com.noman.todolist.service"})
 public class SpringRootConfig {
-    //TODO: Services, DAO, DataSource, Email Sender or some other business layer beans   
+    //TODO: Services, DAO, DataSource, Email Sender or some other business layer beans 
+    
+    
+    // The following bean provide a connection with the database.
+    // When we used JDBC we hard coded for connections everytime we need the connection
+    // Now we write once for the connection, and SpringRootConfig provides the connection for evrytime we nee this. 
     @Bean
     public BasicDataSource getDataSource(){
         BasicDataSource ds = new BasicDataSource();
